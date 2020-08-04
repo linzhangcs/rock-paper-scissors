17f81f1e5365fa407cef252dd3ad8b94030d46d0
Lin Zhang <lin.zhang.ux@gmail.com>
2020-08-03 17:14:03 -0700

    Updates

---
+++ precache-manifest.f694dcb8f60d02121280c6eb86f70f89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d5846eb7b0efccfe59461340ecf10f",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "680407746c1c51a729c5",
+    "url": "/rock-paper-scissors/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d189f6355e2cf52a2b48",
+    "url": "/rock-paper-scissors/static/js/2.006070d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-paper-scissors/static/js/2.006070d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680407746c1c51a729c5",
+    "url": "/rock-paper-scissors/static/js/main.3115d40f.chunk.js"
+  },
+  {
+    "revision": "54d8b3ee6fc233b06722",
+    "url": "/rock-paper-scissors/static/js/runtime-main.5af9a2bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rock-paper-scissors/static/media/logo.5d5d9eef.svg"
+  }
+]);
