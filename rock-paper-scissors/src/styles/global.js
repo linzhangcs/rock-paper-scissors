74c6e30409036ccf3368916ef3bde765b5e8ec05
Lin Zhang <lin.zhang.ux@gmail.com>
2020-08-06 23:42:19 -0700

    add global style variables

---
+++ rock-paper-scissors/src/styles/global.js
@@ -0,0 +1,22 @@
+export const options = {
+    scissorsGradientOne: "hsl(39, 89%, 49%)",
+    scissorsGradientTwo: "hsl(40, 84%, 53%)",
+    paperGradientOne: "hsl(230, 89%, 62%)",
+    paperGradientTwo: "hsl(230, 89%, 65%)",
+    rockGradientOne: "hsl(349, 71%, 52%)",
+    rockGradientTwo: "(349, 70%, 56%)"
+}
+
+export const colors = {
+    white: "#f9f9f9",
+    darkText: "hsl(229, 25%, 31%)",
+    scoreText: "hsl(229, 64%, 46%)",
+    headerOutline: "hsl(217, 16%, 45%)",
+    backgroundOne: "hsl(214, 47%, 23%)",
+    backgroundTwo: "hsl(237, 49%, 15%)"
+}
+
+export const breakpoints = {
+    mobile: "375px",
+    desktop: "1366px"
+}
