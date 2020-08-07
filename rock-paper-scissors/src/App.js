2fcd4bd97041c9e5a5db595ae496783e26f1f880
Lin Zhang <lin.zhang.ux@gmail.com>
2020-08-06 23:41:52 -0700

    add background gradient

--- rock-paper-scissors/src/App.js
+++ rock-paper-scissors/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import RockPaperScissorsGame from './RockPaperScissorsGame';
-
+import styled from 'styled-components';
+import {colors} from './styles/global';
+const Background = styled.div`
+  height: 100vh;
+  background: radial-gradient(circle at top, ${colors.backgroundOne} 18%, ${colors.backgroundTwo} 60%);
+`;
 function App() {
   return (
-    <RockPaperScissorsGame/>
+    <Background>
+      <RockPaperScissorsGame/>
+    </Background>
   );
 }
 
