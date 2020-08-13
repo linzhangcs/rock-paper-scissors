2553023d14d817e4a765920504b756d85e567499
Lin Zhang <lin.zhang.ux@gmail.com>
2020-08-12 20:11:59 -0700

    override selected game options's layout

--- rock-paper-scissors/src/components/GameOptioins.js
+++ rock-paper-scissors/src/components/GameOptioins.js
@@ -7,7 +7,7 @@ import scissors from '../images/icon-scissors.svg';
 
 const GameOption = styled.div`
 font-size: 2em;
-
+transition: all 800ms ease;
 .option{
   background: linear-gradient(180deg, ${options.paperGradientOne}, ${options.paperGradientTwo});
   width: 200px;
@@ -46,17 +46,11 @@ font-size: 2em;
 .scissors{
     background: linear-gradient(180deg, ${options.scissorsGradientOne}, ${options.scissorsGradientTwo});
     box-shadow: 0px 10px ${options.scissorsShadow};
-    // &:hover{
-    //     box-shadow: 0 1px 10px 8px ${options.scissorsShadow};
-    //   }
 }
 
 .rock{
     background: linear-gradient(180deg, ${options.rockGradientOne}, ${options.rockGradientTwo});
     box-shadow: 0px 10px ${options.rockShadow};
-    // &:hover{
-    //     box-shadow: 0 1px 10px 8px ${options.rockShadow};
-    //   }
 }
 `;
 
@@ -73,6 +67,22 @@ const OptionsLayout = styled.div`
     }
   `;
 
+const OptionsSelectedLayout = styled(OptionsLayout)`
+    margin-top: 100px;
+    .rock-wrapper{
+        flex-basis: auto;
+        display: flex;
+        justify-content: center;
+      }
+`;
+
+const SelectedGameOption = styled(GameOption)`
+    .option{
+        .icon-wrapper{
+            cursor: default;
+        }
+    }
+`;
 const Option = ({onClick, option, bg}) => {
     return(
         <GameOption className={`${option}-wrapper`} data-option={option} onClick={(event => onClick(event))}>
@@ -84,8 +94,18 @@ const Option = ({onClick, option, bg}) => {
         </GameOption>
     )
 }
-  
-  
+
+ const SelectedOption = ({option, bg}) => {
+    return(
+        <SelectedGameOption className={`${option}-wrapper`} data-option={option}>
+        <div className={`option ${option}`} data-option={option}>
+            <div className="icon-wrapper" data-option={option}>
+                <img src={bg} data-option={option}/>
+            </div>
+        </div>
+        </SelectedGameOption>
+    )
+ }
 const GameOptions = ({onClick, housePick, userPick, gameOptions}) => {
     let pickedGameOptions = undefined;
     console.log('gameoptions', housePick);
@@ -98,10 +118,7 @@ const GameOptions = ({onClick, housePick, userPick, gameOptions}) => {
         console.log('gameoptions', housePick);
         const userPickBg = gameOptions.filter(option => option.text === userPick)[0].bg;
         let housePickBg = housePick === null ? '' : gameOptions.filter(option => option.text === housePick)[0].bg;    
-        // const housePickBg = '';
-
-
-
+        
         pickedGameOptions= [
             {
                 text: userPick,
@@ -115,12 +132,19 @@ const GameOptions = ({onClick, housePick, userPick, gameOptions}) => {
             console.log(pickedGameOptions);
         }
     }
-    return(
-        <OptionsLayout>
-            {userPick === null && gameOptions.map((option) => { return <Option onClick={(event) => onClick(event)} option={option.text} bg={option.bg}></Option>})}
-            {userPick !== null && pickedGameOptions.map(option => { return <Option option={option.text} bg={option.bg}></Option>} )}
-        </OptionsLayout>
-    )
-}
+        if(userPick === null){
+            return(
+                <OptionsLayout>
+                    {gameOptions.map((option) => { return <Option onClick={(event) => onClick(event)} option={option.text} bg={option.bg}></Option>})}
+                </OptionsLayout>
+            )        
+        }else{
+            return(
+                <OptionsSelectedLayout>
+                    {pickedGameOptions.map(option => { return <SelectedOption option={option.text} bg={option.bg}></SelectedOption>} )}
+                </OptionsSelectedLayout>
+            )
+        }
+    }
 
 export default GameOptions;
